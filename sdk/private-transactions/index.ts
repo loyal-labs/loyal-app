5c9322aa59643e079bc056a26ca9eaca5b6e4c15
Zotho <sviatoslav.alekseev@proton.me>
2026-05-09 00:57:59 +0500

    fix(sdk): export Kamino reserve valuation helpers

--- sdk/private-transactions/index.ts
+++ sdk/private-transactions/index.ts
@@ -103,7 +103,12 @@ export {
   ER_VALIDATOR_MAINNET,
   getErValidatorForSolanaEnv,
   getErValidatorForRpcEndpoint,
+  getKaminoModifyBalanceAccountsForTokenMint,
+  isKaminoMainnetModifyBalanceAccounts,
+  KLEND_PROGRAM_ID,
   PROGRAM_ID,
+  USDC_MINT_DEVNET,
+  USDC_MINT_MAINNET,
   DELEGATION_PROGRAM_ID,
   PERMISSION_PROGRAM_ID,
   MAGIC_PROGRAM_ID,
@@ -120,6 +125,15 @@ export {
   solToLamports,
   lamportsToSol,
 } from "./src/constants";
+
+export {
+  calculateKaminoCollateralExchangeRateSfFromAmounts,
+  calculateKaminoCollateralValuation,
+  calculateKaminoRedeemableLiquidityAmountRaw,
+  calculateKaminoShareAmountForLiquidityAmountRaw,
+  fetchKaminoReserveSnapshot,
+  parseKaminoReserveSnapshotFromAccountData,
+} from "./src/kamino";
 
 // PDA helpers
 export {
