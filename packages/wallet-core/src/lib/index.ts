333f7fd0c651e6a1ac28d0abcac440c745c45cc5
Vlad Arbatov <v.o.arbatov@gmail.com>
2026-07-06 21:47:14 +0300

    fix(wallet-core): drop re-exports of deleted sign-in and constants modules

--- packages/wallet-core/src/lib/index.ts
+++ packages/wallet-core/src/lib/index.ts
@@ -3,6 +3,4 @@ export * from "./jupiter/fee-estimator";
 export * from "./jupiter/types";
 export { getTokenIconUrl } from "./token-icon";
 export * from "./solana/wsol-adapter";
-export * from "./solana/sign-in";
-export * from "./solana/constants";
 export * from "./solana/types";
